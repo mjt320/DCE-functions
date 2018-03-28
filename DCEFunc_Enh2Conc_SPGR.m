c6637ebfbd476379ae6fe84917c7f5ba74174279
mjt320 <36667519+mjt320@users.noreply.github.com>
2018-03-28 16:20:17 +0100

    more elegant implementations

--- DCEFunc_Enh2Conc_SPGR.m
+++ DCEFunc_Enh2Conc_SPGR.m
@@ -14,10 +14,35 @@ conc_mM=nan(size(enhancementPct));
 NTimePoints=size(enhancementPct,1);
 NSeries=size(enhancementPct,2);
 
-S0=1;
-%CTry_mM=[-0.1:0.0001:0.1 0.101:0.001:1 1.01:0.01:8.00]; %calculate enhancement for all trial conc. values and select conc. corresponding to closest enhancement
-CTry_mM=[-0.1:0.00001:0.1 0.1001:0.0001:1 1.001:0.001:8.00]; %calculate enhancement for all trial conc. values and select conc. corresponding to closest enhancement
+S0=1; % this value is generally unknown and doesn't affect enhancement for SPGR
 T2s0_s = 1; % this value is generally unknown and doesn't affect enhancement for SPGR
+
+
+% maxC=8; maxNegC=0.1; minPctInc=2; minAbsInc=0.0001;
+% nMaxPos=log(maxC/minInc)/log(1+pctInc/100);
+% nMaxNeg=log(maxNegC/minInc)/log(1+pctInc/100);
+% CTry_mM=[fliplr(-minInc*(1+pctInc/100).^(1:nMaxNeg)) 0 minInc*(1+pctInc/100).^(1:nMaxPos)]; %calculate enhancement for all trial conc. values and select conc. corresponding to closest enhancement
+% size(CTry_mM)
+
+
+%% calculate concentration values to try
+maxC=8; %maximum concentration
+maxNegC=0.1; %maximum negative concentration
+minPctInc=1; %minimum percent increase from previous value
+minAbsInc=0.0001; %minimum absolute increase from previous value
+CTry_mM=-maxNegC;
+while 1
+    c = CTry_mM(1,end)+max(minAbsInc,(minPctInc/100)*abs(CTry_mM(1,end)));
+    if c>maxC; break; else
+        CTry_mM = [CTry_mM c];
+    end
+end
+
+%size(CTry_mM)
+%CTry_mM=[-0.1:0.0001:0.5 0.51:0.01:8.00]; %calculate enhancement for all trial conc. values and select conc. corresponding to closest enhancement
+%CTry_mM=[fliplr(-0.0001*(1+0.01).^[1:700]) 0.0001*(1+0.01).^(1:1135)]; %calculate enhancement for all trial conc. values and select conc. corresponding to closest enhancement
+%CTry_mM=[-0.1:0.00005:0.1 0.1005:0.0005:1 1.005:0.005:8.00]; %calculate enhancement for all trial conc. values and select conc. corresponding to closest enhancement
+
 
 T2s_try_s = ( T2s0_s^-1 + r2s_permMperS * CTry_mM ).^-1; %create row vector of T2s for each test concentration
 
@@ -32,27 +57,17 @@ for iSeries=1:NSeries %loop through different time series (e.g. voxels or ROIs)
     T1_try_filt_s= T1_try_s(T1_try_s > 0); % select only test concentrations that lead to positive T1 values
     T2s_try_filt_s = T2s_try_s(T1_try_s > 0);
     CTry_filt_mM = CTry_mM(T1_try_s > 0);
+    NTry_filt=size(CTry_filt_mM,2);
     
-        
     predictedSignal_post = DCEFunc_getSPGRSignal(S0,T1_try_filt_s,T2s_try_filt_s,TR_s,TE_s,FA_deg(1,iSeries)); %calculate row vector of signals with a value for each test concentration
     predictedEnhancementPct = 100 * ( (predictedSignal_post - predictedSignal_pre) ./ predictedSignal_pre ); %calculed predicted enhancements
     
-    for iTimePoint=1:NTimePoints %loop through time points
-        
-        diff = abs(predictedEnhancementPct - enhancementPct(iTimePoint,iSeries)); %absolute difference between predicted and actual enhancement for each test conc
-        [y,minIdx]=min(diff); % choose test conc with min difference
-        conc_mM(iTimePoint,iSeries)=CTry_filt_mM(minIdx); % set concentration value
-        
-    end
+    diff = abs(repmat(predictedEnhancementPct,[NTimePoints 1]) - repmat(enhancementPct(:,iSeries),[1 NTry_filt])); %absolute difference between predicted and actual enhancement for each test conc and time point
+    [y,minIdx]=min(diff.'); % choose test concentration values with min difference for each time point
+    conc_mM(:,iSeries)=CTry_filt_mM(minIdx); % set concentration value for each time point
+    
 end
 
 end
 
 
-%% local function to predict SPGR signal
-% function SPGRSignal = getSPGRSignal(S0,T1_s,T2s_s,TR_s,TE_s,beta_deg)
-% %beta_rad = 2*pi*(beta_deg/360);
-% SPGRSignal = S0 .* ...
-%     (((1-exp(-TR_s./T1_s))*sin(2*pi*(beta_deg/360))) ./ (1-exp(-TR_s./T1_s)*cos(2*pi*(beta_deg/360))) ) ...
-%     .* exp(-TE_s./T2s_s) ;
-% end
--- DCEFunc_fitModel.m
+++ DCEFunc_fitModel.m
@@ -66,6 +66,25 @@ switch model
             CtModelFit_mM(:,iSeries)=[nan(opts.NIgnore,1); CtModelFit_mM(opts.NIgnore+1:end,iSeries)];
         end
         
+    case 'PatlakFast'
+        %linear Patlak using Cp and Integral Cp as regressors - vP and K are the regressors
+        %uses matrix divide so should be fast
+        
+        PKP.vP=nan(1,N);
+        PKP.PS_perMin=nan(1,N);
+        intCp_AIF_mM_min=nan(NTime,1);
+        beta=nan(2,N);
+        
+        for iFrame=1:NTime %(calculate integral to use as regressor)
+            intCp_AIF_mM_min(iFrame,1)=(0.5*Cp_AIF_mM(iFrame) + sum(Cp_AIF_mM(1:iFrame-1,1),1)) * (tRes_s/60);
+        end
+        
+        reg=[Cp_AIF_mM intCp_AIF_mM_min];
+        beta(:,:) = reg \ Ct_mM;
+        
+        PKP.vP(1,:)=beta(1,:);
+        PKP.PS_perMin=beta(2,:);
+        
 end
 
 
