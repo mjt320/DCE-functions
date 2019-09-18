653af72a994974116f32b51e583ba06a7e5dd2a6
Jose Bernal <jose.bernal@correounivalle.edu.co>
2019-09-18 13:44:37 +0100

    Faster DCEFunc_Conc2Enh_SPGR
    For loops are not really necessary. I propose taking advantage of matrix operations instead to fasten the process.

--- DCEFunc_Conc2Enh_SPGR.m
+++ DCEFunc_Conc2Enh_SPGR.m
@@ -11,23 +11,12 @@ function enhancementPct=DCEFunc_Conc2Enh_SPGR(conc_mM,T10_s,TR_s,TE_s,FA_deg,r1_
 % r1_permMperS, r2_permMperS: T1 and T2 relaxivity values. Ignores T2'
 % effects.
 
-enhancementPct=nan(size(conc_mM));
-
-NTimePoints=size(conc_mM,1);
-NSeries=size(conc_mM,2);
-
 S0=1; % this value doesn't affect enhancement for SPGR
 T2s0_s = 1; % this value doesn't affect enhancement for SPGR
-
-for iSeries=1:NSeries %loop through different time series (e.g. voxels or ROIs)
-    signal_pre = DCEFunc_getSPGRSignal(S0,T10_s(1,iSeries),T2s0_s,TR_s,TE_s,FA_deg(1,iSeries)); %calculate pre-contrast signal
-    for iTimePoint=1:NTimePoints %loop through time points
-        T2s_s = ( T2s0_s^-1 + r2s_permMperS * conc_mM(iTimePoint,iSeries) ).^-1; %calculate T2* post-enhancement
-        T1_s = ( T10_s(1,iSeries)^-1 + r1_permMperS * conc_mM(iTimePoint,iSeries) ).^-1; %calculate T1 post-enhancement
-        
-        signal_post = DCEFunc_getSPGRSignal(S0,T1_s,T2s_s,TR_s,TE_s,FA_deg(1,iSeries)); %calculate post-contrast signal
-        enhancementPct(iTimePoint,iSeries) = 100 * ( (signal_post - signal_pre) / signal_pre ); %calculate enhancement
-    end
-end
+signal_pre = DCEFunc_getSPGRSignal(S0,T10_s,T2s0_s,TR_s,TE_s,FA_deg); %calculate pre-contrast signal
+T2s_s = ( T2s0_s.^-1 + r2s_permMperS * conc_mM ).^-1; %calculate T2* post-enhancement
+T1_s = ( T10_s.^-1 + r1_permMperS * conc_mM ).^-1; %calculate T1 post-enhancement
+signal_post = DCEFunc_getSPGRSignal(S0,T1_s,T2s_s,TR_s,TE_s,FA_deg); %calculate post-contrast signal
+enhancementPct = 100 * ( (signal_post - signal_pre) ./ signal_pre ); %calculate enhancement
 
 end
