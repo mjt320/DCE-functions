f563bb9bc1f4d5fd74a0b6d04c9d59794200654a
CManning95 <43373756+CManning95@users.noreply.github.com>
2019-12-13 16:07:10 +0000

    Update DCEFunc_getParkerModAIF.m

--- DCEFunc_getParkerModAIF.m
+++ DCEFunc_getParkerModAIF.m
@@ -1,4 +1,4 @@
-function [t_s,Cp_AIF_mM]=DCEFunc_getParkerModAIF(t_res_s,t_acq_s,t_start_s,Hct)
+function [t_s,Cp_AIF_mM]=DCEFunc_getParkerModAIF(t_res_s,t_acq_s,t_start_s,Hct, varargin)
 %Returns a modified version of population average plasma AIF (Parker 2006) in mMol and at time points t
 %Modifications make decay bi-exponential to more accurately represent typical data over a 20-minute acquisition, as described in Heye et al., Neuroimage (2016)
 %Useful for simulations.
@@ -12,6 +12,9 @@ function [t_s,Cp_AIF_mM]=DCEFunc_getParkerModAIF(t_res_s,t_acq_s,t_start_s,Hct)
 %t_acq_s=duration of acquisition
 %t_start_s=time of start of injection (applies a time shift to the modified Parker function)
 %Hct=hematocrit (this converts the Cb values determined by the (modified) Parker function to Cp values)
+%-- variable inputs --
+% 'MSS2' or 'MSS3' - changes aplha and beta values to match decay according to
+% patients from these studies
 
 t_s=((t_res_s/2):t_res_s:(t_acq_s-t_start_s)).'; %calculate time points (starting at t_res/2)
 t_min=t_s/60; %Parker function uses minute units, so convert time;
@@ -23,7 +26,19 @@ sigma1=0.0563; sigma2=0.132;
 s=38.078; tau=0.483;
 %alpha=1.050; beta=0.1685; %(original Parker values)
 alpha=3.1671; beta=1.0165;
-alpha2=0.5628; beta2=0.0266;
+
+% take decay_type from variable input, if none then assign MSS2 values
+if isempty(varargin) == 1;
+    alpha2=0.5628; beta2=0.0266; % default to MSS2 values
+elseif isempty(varargin) == 0;
+    decay_type = varargin{1};
+    switch decay_type
+    case 'MSS2' % matches MSS2 patients with Dotarem
+        alpha2=0.5628; beta2=0.0266;
+    case 'MSS3' % matches MSS3 patients with Gadovist
+        alpha2=0.765; beta2=0.0325;
+    end
+end
 
 Cb_mM=(A1/(sigma1*sqrt(2*pi)))*exp(-((t_min-T1).^2)/(2*sigma1^2)) + ... %calculate Cb
     (A2/(sigma2*sqrt(2*pi)))*exp(-((t_min-T2).^2)/(2*sigma2^2)) + ...
